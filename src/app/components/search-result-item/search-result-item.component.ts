ca35147764deb2d467d6aa74a04b9b819eaf3ae4
NexGenUA <mr.rinus@gmail.com>
2020-08-08 17:36:15 +0300

    feat: add card component

---
+++ src/app/components/search-result-item/search-result-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { SearchItem } from '../../shared/models/search-item.model';
+
+@Component({
+  selector: 'app-search-result-item',
+  templateUrl: './search-result-item.component.html',
+  styleUrls: ['./search-result-item.component.scss']
+})
+export class SearchResultItemComponent {
+  @Input() public item: SearchItem;
+}
