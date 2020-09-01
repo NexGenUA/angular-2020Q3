7ed1652856544518218c49a7ab9a9bc026b414f3
NexGenUA <mr.rinus@gmail.com>
2020-09-01 21:48:12 +0300

    refactor: structure

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
-import { YoutubeModule } from '../youtube/youtube.module';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { YoutubeModule } from '../youtube/youtube.module';
 
 @NgModule({
-  declarations: [HeaderComponent, NotFoundComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    SharedModule,
-    YoutubeModule,
-  ],
+  declarations: [HeaderComponent, NotFoundComponent, FilterComponent],
+  imports: [SharedModule, YoutubeModule],
   exports: [
     HeaderComponent,
-    YoutubeModule,
+    FilterComponent,
   ]
 })
 export class CoreModule { }
--- src/app/youtube/youtube.module.ts
+++ src/app/youtube/youtube.module.ts
@@ -1,12 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { FilterComponent } from './components/filter/filter.component';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
 import { SearchResultItemComponent } from './components/search-result-item/search-result-item.component';
-import { BorderColorDirective } from '../shared/directives/border-color.directive';
-import { ReduceNumberPipe } from '../shared/pipes/reduce-number.pipe';
-import { SortPipe } from '../shared/pipes/sort.pipe';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { InfoBarComponent } from './components/info-bar/info-bar.component';
 import { DetailedInfoComponent } from './components/detailed-info/detailed-info.component';
@@ -14,24 +8,11 @@ import { DetailedInfoComponent } from './components/detailed-info/detailed-info.
 @NgModule({
   declarations: [
     SearchResultsComponent,
-    FilterComponent,
     SearchResultItemComponent,
-    ReduceNumberPipe,
-    SortPipe,
-    BorderColorDirective,
     InfoBarComponent,
     DetailedInfoComponent,
   ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    SharedModule,
-  ],
-  exports: [
-    SearchResultsComponent,
-    FilterComponent,
-    SearchResultItemComponent,
-  ]
+  imports: [SharedModule]
 })
 export class YoutubeModule {
 }
